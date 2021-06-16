29748b9f5c7fb15bcfc0d9d14cec9600f8cd5904
Jonathan Sterling <jon@jonmsterling.com>
2021-06-16 11:05:43 -0400

    tweaks

--- DcpoExponential.v
+++ DcpoExponential.v
@@ -1,106 +1,113 @@
 Require Import Preamble Preorder Poset Dcpo.
-Context (D E : Dcpo.type).
 
-Definition map := {f : D → E | is_continuous f}.
-Definition ap (f : map) : D → E := proj1_sig f.
-Definition ap_cont (f : map) : is_continuous (ap f) := proj2_sig f.
+Section Map.
+  Context (D E : Dcpo.type).
 
-Lemma map_ext : ∀ f g, ap f = ap g → f = g.
-Proof.
-  rewrite /map.
-  move=> f g fg.
-  apply: eq_sig.
-  apply: proof_irrelevance.
-Qed.
+  Definition map := {f : D → E | is_continuous f}.
+  Definition ap (f : map) : D → E := proj1_sig f.
+  Definition ap_cont (f : map) : is_continuous (ap f) := proj2_sig f.
 
-Definition map_lt (f g : map) : Prop :=
-  ∀ x, ap f x ≤ ap g x.
-
-Lemma map_ltR : ∀ f, map_lt f f.
-Proof. move=> f x; apply: ltR. Qed.
-
-Lemma map_ltT : ∀ f g h, map_lt f g → map_lt g h → map_lt f h.
-Proof.
-  move=> f g h fg gh x.
-  apply: ltT.
-  - apply: fg.
-  - apply: gh.
-Qed.
-
-HB.instance Definition map_preorder_axioms := PreorderOfType.Build map map_lt map_ltR map_ltT.
-
-Lemma map_ltE : ∀ f g : map, f ≤ g → g ≤ f → f = g.
-Proof.
-  move=> f g fg gf.
-  apply: map_ext.
-  extensionality x.
-  apply: ltE.
-  - apply: fg.
-  - apply: gf.
-Qed.
-
-HB.instance Definition map_poset_axioms := PosetOfPreorder.Build map map_ltE.
-
-Section Lub.
-
-  Context (A : Family map).
-
-  Lemma push_ap_directed : ∀ (x : D), is_directed A → is_directed (push_fam (λ f, ap f x) A).
+  Lemma map_ext : ∀ f g, ap f = ap g → f = g.
   Proof.
-    move=> x dir.
-    split.
-    - apply: nonempty dir.
-    - move=> //= i j.
-      case: (predirected A dir i j) => k [ij jk].
-      exists k; repeat split.
-      + apply: ij.
-      + apply: jk.
+    rewrite /map.
+    move=> f g fg.
+    apply: eq_sig.
+    apply: proof_irrelevance.
   Qed.
 
-  Section Map.
-    Context (dir : is_directed A).
+  Definition map_lt (f g : map) : Prop :=
+    ∀ x, ap f x ≤ ap g x.
 
-    Definition dlub_fun : D → E :=
-      λ x,
-      dlub (push_fam (λ f, ap f x) A) (push_ap_directed x dir).
+  Lemma map_ltR : ∀ f, map_lt f f.
+  Proof. move=> f x; apply: ltR. Qed.
 
-    Lemma dlub_fun_continuous : is_continuous dlub_fun.
+  Lemma map_ltT : ∀ f g h, map_lt f g → map_lt g h → map_lt f h.
+  Proof.
+    move=> f g h fg gh x.
+    apply: ltT.
+    - apply: fg.
+    - apply: gh.
+  Qed.
+
+  HB.instance Definition map_preorder_axioms := PreorderOfType.Build map map_lt map_ltR map_ltT.
+
+  Lemma map_ltE : ∀ f g : map, f ≤ g → g ≤ f → f = g.
+  Proof.
+    move=> f g fg gf.
+    apply: map_ext.
+    extensionality x.
+    apply: ltE.
+    - apply: fg.
+    - apply: gf.
+  Qed.
+
+  HB.instance Definition map_poset_axioms := PosetOfPreorder.Build map map_ltE.
+
+  Section Lub.
+
+    Context (A : Family map).
+
+    Lemma push_ap_directed : ∀ (x : D), is_directed A → is_directed (push_fam (λ f, ap f x) A).
     Proof.
-      move=> F dirF; split.
-      - move=> //= i.
-        apply: above_lub.
-        + apply: dlub_is_lub.
-        + move=> //= z.
-          apply: ltT.
-          * apply: continuous_to_monotone.
-            -- by apply: ap_cont.
-            -- by apply: dlub_is_ub.
-          * apply: (dlub_is_ub (push_fam _ A)).
-      - move=> z //= H.
-        apply: dlub_least.
-        move=> //= x.
-        apply: lub_univ.
-        + apply: ap_cont.
-        + move=> //= y.
-          apply: ltT'.
-          * by apply: H.
-          * by apply: (dlub_is_ub (push_fam _ A) _).
+      move=> x dir.
+      split.
+      - apply: nonempty dir.
+      - move=> //= i j.
+        case: (predirected A dir i j) => k [ij jk].
+        exists k; repeat split.
+        + apply: ij.
+        + apply: jk.
     Qed.
 
-    Lemma map_ltHasDLubs : ∃ f, is_lub A f.
-    Proof.
-      unshelve esplit.
-      - unshelve esplit.
-        + by apply: dlub_fun.
-        + by apply: dlub_fun_continuous.
-      - split; simpl.
-        + move=> i; move=> ?.
-          apply: (dlub_is_ub (push_fam _ A) _) i.
-        + move=> f Hf; move=> ?.
-          apply: dlub_least => ?.
-          apply: Hf.
-    Qed.
-  End Map.
-End Lub.
+    Section Map.
+      Context (dir : is_directed A).
 
-HB.instance Definition map_dcpo_axioms := DcpoOfPoset.Build map map_ltHasDLubs.
+      Definition dlub_fun : D → E :=
+        λ x,
+        dlub (push_fam (λ f, ap f x) A) (push_ap_directed x dir).
+
+      Lemma dlub_fun_continuous : is_continuous dlub_fun.
+      Proof.
+        move=> F dirF; split.
+        - move=> //= i.
+          apply: above_lub.
+          + apply: dlub_is_lub.
+          + move=> //= z.
+            apply: ltT.
+            * apply: continuous_to_monotone.
+              -- by apply: ap_cont.
+              -- by apply: dlub_is_ub.
+            * apply: (dlub_is_ub (push_fam _ A)).
+        - move=> z //= H.
+          apply: dlub_least.
+          move=> //= x.
+          apply: lub_univ.
+          + apply: ap_cont.
+          + move=> //= y.
+            apply: ltT'.
+            * by apply: H.
+            * by apply: (dlub_is_ub (push_fam _ A) _).
+      Qed.
+
+      Lemma map_ltHasDLubs : ∃ f, is_lub A f.
+      Proof.
+        unshelve esplit.
+        - unshelve esplit.
+          + by apply: dlub_fun.
+          + by apply: dlub_fun_continuous.
+        - split; simpl.
+          + move=> i; move=> ?.
+            apply: (dlub_is_ub (push_fam _ A) _) i.
+          + move=> f Hf; move=> ?.
+            apply: dlub_least => ?.
+            apply: Hf.
+      Qed.
+    End Map.
+  End Lub.
+
+  HB.instance Definition map_dcpo_axioms := DcpoOfPoset.Build map map_ltHasDLubs.
+
+End Map.
+
+
+Arguments ap [D] [E].
--- Sierpinski.v
+++ Sierpinski.v
@@ -16,14 +16,18 @@ Proof. move=> *; apply: propositional_extensionality; by split. Qed.
 
 HB.instance Definition Σ_poset_axioms := PosetOfPreorder.Build Σ Σ_ltE.
 
+Lemma Σ_exists_is_lub : ∀ (A : Family Σ), is_lub A (∃ x, A x).
+  move=> A; split.
+  - by compute; eauto.
+  - move=> z zub; move=> [? ?].
+    by apply: zub; eauto.
+Qed.
+
 Lemma Σ_ltHasDLubs : ∀ (A : Family Σ), is_directed A → ∃ x, is_lub A x.
 Proof.
   move=> A dir //=.
   exists (∃ x, A x).
-  split; simpl.
-  - by compute; eauto.
-  - move=> z zub; move=> [? ?].
-      by apply: zub; eauto.
+  apply: Σ_exists_is_lub.
 Qed.
 
 HB.instance Definition Σ_dcpo_axioms := DcpoOfPoset.Build Σ Σ_ltHasDLubs.
@@ -36,3 +40,15 @@ Proof. exists True; by move=> ?. Qed.
 
 HB.instance Definition Σ_pointed_poset_axioms := PointedPosetOfPoset.Build Σ Σ_ltHasBot.
 HB.instance Definition Σ_bounded_poset_axioms := BoundedPosetOfPointedPoset.Build Σ Σ_ltHasTop.
+
+Lemma Σ_top_rw : (⊤ : Σ) = True.
+  apply: top_is_unique.
+  - apply: top_is_top.
+  - done.
+Qed.
+
+Lemma Σ_bot_rw : (⊥ : Σ) = False.
+  apply: bottom_is_unique.
+  - apply: bottom_is_bottom.
+  - by compute.
+Qed.
