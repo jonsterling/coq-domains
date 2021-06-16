3d1c6f8a3347a578091d229fe0930d1864b9fa1b
Jonathan Sterling <jon@jonmsterling.com>
2021-06-16 15:35:06 -0400

    for fun, add image factorization of types

--- theories/Preamble.v
+++ theories/Preamble.v
@@ -1,6 +1,6 @@
 Require Export ssreflect Unicode.Utf8.
 From HB Require Export structures.
-Require Export Coq.Logic.Description Coq.Logic.PropExtensionality Coq.Logic.FunctionalExtensionality.
+Require Export Coq.Logic.Description Coq.Logic.PropExtensionality Coq.Logic.FunctionalExtensionality Program.Equality.
 
 Set Primitive Projections.
 
@@ -9,3 +9,27 @@ Definition iota {X : Type} (P : X → Prop) (h : exists! x, P x) : X :=
 
 Lemma iota_prop {X : Type} (P : X → Prop) (h : exists! x, P x) : P (iota P h).
 Proof. rewrite /iota; apply proj2_sig. Qed.
+
+
+Module Im.
+  Section Im.
+    Context {X Y : Type} (f : X → Y).
+
+    Definition T : Type :=
+      { y : Y | ∃ x, f x = y }.
+
+    Definition surj : X → T.
+    Proof. move=> x; exists (f x); by exists x. Defined.
+
+    Definition inj : T → Y.
+    Proof. apply: proj1_sig. Defined.
+
+    Lemma inj_injective : ∀ x y, inj x = inj y → x = y.
+    Proof. move=> x y h; apply: eq_sig; apply: proof_irrelevance. Qed.
+
+    Lemma surj_surjective : ∀ i : T, ∃ x : X, surj x = i.
+    Proof. move=> [y [x h]]; exists x; apply: eq_sig; apply: proof_irrelevance. Qed.
+  End Im.
+End Im.
+
+Notation Im := Im.T.
