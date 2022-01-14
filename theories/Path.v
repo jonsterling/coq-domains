50949125ad4e346d676d8281719db6fd728bd7a5
Jon Sterling <jon@jonmsterling.com>
2022-01-14 19:00:04 +0100

    prove the Phoa Principle for constructive domain theory

--- theories/Path.v
+++ theories/Path.v
@@ -1,98 +1,193 @@
 From Domains Require Import Preamble Preorder Poset Dcpo Sierpinski DcpoExponential.
 
-Context (D : Dcpo.type).
+Section Path.
+  Context (D : Dcpo.type).
 
-Definition Path := map (HB.pack Σ) D.
+  Definition Path := map (HB.pack Σ) D.
 
-Lemma lub_intro (A : Family Σ): ∀ u ϕ, is_lub A ϕ → A u → ϕ.
-Proof. by move=> u ϕ ϕlub; apply: (lub_is_ub A). Qed.
+  Lemma lub_intro (A : Family Σ): ∀ u ϕ, is_lub A ϕ → A u → ϕ.
+  Proof. by move=> u ϕ ϕlub; apply: (lub_is_ub A). Qed.
 
-Section PathFromLt.
-  Context (x y : D) (xy : x ≤ y).
+  Section PathFromLt.
+    Context (x y : D) (xy : x ≤ y).
 
-  Section Family.
-    Context (ϕ : Σ).
+    Section Family.
+      Context (ϕ : Σ).
 
-    Definition path_fam : Family D.
+      Definition path_fam : Family D.
+      Proof.
+        exists (sum ϕ True).
+        by case=> _; [exact: y| exact: x].
+      Defined.
+
+      Lemma path_fam_directed : is_directed path_fam.
+      Proof.
+        split.
+        - by exists (inr I).
+        - by case=>a1; case=>a2 /=; (unshelve esplit; [left + right; assumption | eauto]).
+      Qed.
+    End Family.
+
+    Lemma dlub_continuous : is_continuous (λ ϕ : Σ, dlub (path_fam ϕ) (path_fam_directed ϕ)).
     Proof.
-      exists (sum ϕ True).
-      by case=> _; [exact: y| exact: x].
-    Defined.
-
-    Lemma path_fam_directed : is_directed path_fam.
-    Proof.
-      split.
-      - by exists (inr I).
-      - by case=>a1; case=>a2 /=; (unshelve esplit; [left + right; assumption | eauto]).
-    Qed.
-  End Family.
-
-  Lemma dlub_continuous : is_continuous (λ ϕ : Σ, dlub (path_fam ϕ) (path_fam_directed ϕ)).
-  Proof.
-    move=> A dirA p plub; split.
-    - move=> //= u; apply: (above_lub _)=>//=; case.
-      + move=>s; apply: ltT'.
-        * by apply: dlub_is_ub; left; apply: (Σ_lub_intro A _ _ _ s).
-        * by [].
-      + move=> []; apply: ltT'.
-        * by apply: dlub_is_ub; right.
-        * by [].
-    - move=> z zub; apply: (lub_univ _)=>//; case=>/=.
-      + move=> h.
-        suff: ∃ i, A i.
-        * move=> [i u].
-          apply: ltT; last by apply: zub.
+      move=> A dirA p plub; split.
+      - move=> //= u; apply: (above_lub _)=>//=; case.
+        + move=>s; apply: ltT'.
+          * by apply: dlub_is_ub; left; apply: (Σ_lub_intro A _ _ _ s).
+          * by [].
+        + move=> []; apply: ltT'.
+          * by apply: dlub_is_ub; right.
+          * by [].
+      - move=> z zub; apply: (lub_univ _)=>//; case=>/=.
+        + move=> h.
+          suff: ∃ i, A i.
+          * move=> [i u].
+            apply: ltT; last by apply: zub.
+            apply: ltT'.
+            -- by apply: lub_is_ub; [apply: dlub_is_lub | left].
+            -- by [].
+          * apply: (lub_univ A); auto.
+            -- by apply/lub_is_ub/Σ_exists_is_lub.
+            -- suff: dlub A dirA = p by move=>->.
+               by apply: lub_unique.
+        + move=> _.
+          case: dirA => [[i _] h].
+          apply/ltT/zub; last by [].
           apply: ltT'.
-          -- by apply: lub_is_ub; [apply: dlub_is_lub | left].
-          -- by [].
-        * apply: (lub_univ A); auto.
-          -- by apply/lub_is_ub/Σ_exists_is_lub.
-          -- suff: dlub A dirA = p by move=>->.
-             by apply: lub_unique.
-      + move=> _.
-        case: dirA => [[i _] h].
-        apply/ltT/zub; last by [].
-        apply: ltT'.
-        * by apply: dlub_is_ub; right.
+          * by apply: dlub_is_ub; right.
+          * by [].
+    Qed.
+
+    Definition make_path : Path.
+    Proof.
+      unshelve esplit.
+      - move=> ϕ.
+        by apply/dlub/path_fam_directed/ϕ.
+      - by apply: dlub_continuous.
+    Defined.
+  End PathFromLt.
+
+
+
+
+  Definition HasPath x y := ∃ p : Path, ap p ⊥ = x /\ ap p ⊤ = y.
+  Infix "⟿" := HasPath (at level 10).
+
+  Lemma fwd : ∀ x y : D, x ≤ y → x ⟿ y.
+  Proof.
+    move=> x y xy.
+    exists (make_path x y xy); split=>/=.
+    - apply: (lub_unique (path_fam _ _ _))=>//; split.
+      + by case=>//; rewrite Σ_bot_rw.
+      + move=> z h; apply: ltT'.
+        * by apply: h; right.
         * by [].
+    - apply:  (lub_unique (path_fam _ _ _))=>//; split; first by case.
+      move=> z h; apply: ltT'.
+      * by apply: h; left; rewrite Σ_top_rw.
+      * by [].
   Qed.
 
-  Definition make_path : Path.
+  Lemma bwd : ∀ x y : D, x ⟿ y → x ≤ y.
   Proof.
-    unshelve esplit.
-    - move=> ϕ.
-      by apply/dlub/path_fam_directed/ϕ.
-    - by apply: dlub_continuous.
-  Defined.
-End PathFromLt.
+    move=> ? ? [? [<- <-]].
+    by apply/cont_mono/bottom_is_bottom/ap_cont.
+  Qed.
+
+  Lemma characterization : ∀ x y, (x ≤ y) = (x ⟿ y).
+  Proof.
+    move=> x y.
+    by apply: propext; split; [apply: fwd | apply: bwd].
+  Qed.
+
+End Path.
 
 
-Definition HasPath x y := ∃ p : Path, ap p ⊥ = x /\ ap p ⊤ = y.
-Infix "⟿" := HasPath (at level 10).
 
-Lemma fwd : ∀ x y : D, x ≤ y → x ⟿ y.
+Notation "Σ^Σ" := (map (HB.pack Σ) (HB.pack Σ)).
+Notation "Σ/=>" := {ϕ : Σ & {ψ : Σ | ϕ ≤ ψ}}.
+
+Definition phoa_fwd : Σ^Σ → Σ/=>.
 Proof.
-  move=> x y xy.
-  exists (make_path x y xy); split=>/=.
-  - apply: (lub_unique (path_fam _ _ _))=>//; split.
-    + by case=>//; rewrite Σ_bot_rw.
-    + move=> z h; apply: ltT'.
-      * by apply: h; right.
-      * by [].
-  - apply:  (lub_unique (path_fam _ _ _))=>//; split; first by case.
-    move=> z h; apply: ltT'.
-    * by apply: h; left; rewrite Σ_top_rw.
-    * by [].
+  move=> F.
+  exists (ap F ⊥).
+  exists (ap F ⊤).
+  apply: cont_mono=>//=.
+  by apply: svalP.
+Defined.
+
+Definition phoa_bwd : Σ/=> → Σ^Σ.
+Proof.
+  move=> [ϕ [ψ h]].
+  apply: make_path h.
+Defined.
+
+Definition phoa_bwd_fwd : ∀ F, phoa_bwd (phoa_fwd F) = F.
+Proof.
+  move=> F.
+  rewrite /phoa_fwd /phoa_bwd.
+  apply: eq_sig=>//=.
+  apply: funext=> ϕ.
+  apply: propext; split.
+  - apply: (Σ_lub_elim (dlub_is_lub _ _)).
+    case=>//=.
+    + move=> u v.
+      suff: (ap F ⊤ ≤ ap F ϕ); first by apply.
+      by apply: cont_mono; first by apply: svalP.
+    + move=> _ u.
+      suff: (ap F ⊥ ≤ ap F ϕ); first by apply.
+      by apply: cont_mono; first by apply: svalP.
+  - move=> x.
+
+    set fam := path_fam (HB.pack Σ) ⊥ ⊤ ϕ.
+    set Ffam := path_fam (HB.pack Σ) (ap F ⊥) (ap F ⊤) ϕ.
+    set dfam := path_fam_directed (HB.pack Σ )⊥ ⊤ (top_is_top ⊥) ϕ.
+
+    pose Q := (ap F (dlub fam dfam)).
+    match goal with
+    | [|- ?G] =>
+      replace G with Q
+    end.
+    + suff: (ap F ϕ ≤ Q).
+      * by apply.
+      * apply: cont_mono; first by apply: svalP.
+        move=> u.
+        unshelve apply: (Σ_lub_intro _ _ _ (dlub_is_lub _ _)).
+        -- by left.
+        -- by rewrite //= Σ_top_rw.
+    + rewrite /Q; clear Q.
+
+      apply: lub_unique=>//=.
+      have R := (svalP F fam dfam (dlub fam dfam) (dlub_is_lub _ (path_fam_directed _ _ _ _ _))).
+      cbn in R.
+      rewrite (_ : Ffam = push_fam (ap F) fam); last by [].
+      rewrite /Ffam.
+      rewrite /push_fam /path_fam //=.
+      congr Build_Family.
+      apply: funext; case=>//=.
 Qed.
 
-Lemma bwd : ∀ x y : D, x ⟿ y → x ≤ y.
+Definition phoa_fwd_bwd : ∀ x, phoa_fwd (phoa_bwd x) = x.
 Proof.
-  move=> ? ? [? [<- <-]].
-  by apply/cont_mono/bottom_is_bottom/ap_cont.
-Qed.
-
-Lemma characterization : ∀ x y, (x ≤ y) = (x ⟿ y).
-Proof.
-  move=> x y.
-  by apply: propext; split; [apply: fwd | apply: bwd].
+  move=> [ϕ [ψ h]].
+  rewrite /phoa_fwd //=.
+  apply: eq_sigT=>//=.
+  - set fam := path_fam (HB.pack Σ) ϕ ψ ⊥.
+    set dfam := path_fam_directed (HB.pack Σ ) ϕ ψ h ⊥.
+    apply: lub_unique=>//=.
+    split.
+    + case=>//=.
+      by rewrite Σ_bot_rw.
+    + move=>//= χ h'.
+      apply: h' (inr _).
+      done.
+  - move=> H.
+    apply: eq_sig=>//=.
+    simplify_eqs.
+    apply: lub_unique=>//=.
+    split.
+    + by case=>//=.
+    + move=> //= χ h'.
+      apply: h' (inl _).
+      by rewrite Σ_top_rw.
 Qed.
